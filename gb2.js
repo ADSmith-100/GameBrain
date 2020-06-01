ae306de5922520917efb7b9727f25e5a79beaf41
Adam Smith <coloradoasmith@gmail.com>
2020-06-01 14:16:44 -0600

    file structure change

---
+++ gb2.js
@@ -0,0 +1,119 @@
+const searchURL = "https://api.rawg.io/api/games?";
+
+function getGames(search, page_size = 21) {
+  const params = {
+    search: search,
+    page_size: page_size,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayResults(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params).map(
+    (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+  );
+  return queryItems.join("&");
+}
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $(".results").empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.results.length; i++) {
+    $(".results").append(
+      `
+      <div class="games">
+        <h2>${responseJson.results[i].name}</h2>
+        <img src="${responseJson.results[i].background_image}">
+        <p>Release Date: ${responseJson.results[i].released}</p>
+        <p>Metacritic Score: ${responseJson.results[i].metacritic}</p>
+        <a href="#" id="${responseJson.results[i].id}" class="more-info">More Info</a>
+      </div>
+     `
+    );
+  }
+  $("#results").removeClass("hidden");
+}
+
+//$("button").click((e) => {
+// e.preventDefault();
+// const q = $("#search").val();
+//fetch(`https://api.rawg.io/api/games?${q}`)
+// .then((res) => res.json())
+//  .then((games) => {
+//loop through and append to results
+//  });
+//});
+
+function displayMoreInfo(game) {
+  console.log({ game });
+
+  $(".modal h2").text(game.name);
+  $(".modal .modal-image")
+    .css("background-image", `url('${game.background_image}')`)
+    .removeClass("loading");
+}
+
+function getMoreInfo(id) {
+  fetch(`https://api.rawg.io/api/games/${id}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => displayMoreInfo(responseJson))
+    .catch((err) => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
+}
+
+$(".close").click(closeModal);
+$(".overlay").click(closeModal);
+
+function closeModal(e) {
+  e.preventDefault();
+  $(".modal").hide();
+  $(".overlay").hide();
+}
+
+function watchForm() {
+  $("form").submit((event) => {
+    event.preventDefault();
+    const game = $("#search").val();
+    //const searchTerm = $("#js-search-term").val();
+    //const maxResults = $("#js-max-results").val();
+    getGames(game);
+  });
+}
+
+function watchMoreInfo() {
+  $(".results").on("click", ".more-info", (e) => {
+    e.preventDefault();
+    $(".modal-image").addClass("loading");
+    $(".modal h2").text("Loading...");
+    $(".modal").show();
+    $(".overlay").show();
+    const id = e.target.id;
+    getMoreInfo(id);
+  });
+}
+
+$(watchForm);
+$(watchMoreInfo);
